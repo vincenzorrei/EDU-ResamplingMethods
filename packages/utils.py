8e9d8f608d3230e9cce7fd99005a1d39d7805dfe
vincenzorrei <vin.orrei@gmail.com>
2025-03-25 23:37:09 +0100

    enhance graphs

--- packages/utils.py
+++ packages/utils.py
@@ -16,8 +16,10 @@ def plot_kfold(cv, X, y, ax, n_splits, xlim_max=100):
     xlim_max: Maximum limit for the x-axis
     """
 
-    # Set color map for the plot
-    cmap_cv = plt.cm.coolwarm
+    # Define colors
+    training_color = 'gold'
+    validation_color = 'forestgreen'
+    
     cv_split = cv.split(X=X, y=y)
 
     for i_split, (train_idx, test_idx) in enumerate(cv_split):
@@ -25,11 +27,15 @@ def plot_kfold(cv, X, y, ax, n_splits, xlim_max=100):
         indices = np.full(len(X), np.nan)
         indices[test_idx], indices[train_idx] = 1, 0
 
+        # Create colors array
+        colors = np.array([validation_color if idx == 1 else training_color for idx in indices])
+        colors[np.isnan(indices)] = 'none'  # Set NaN indices to transparent
+        
         # Plot the training and testing indices
         ax_x = range(len(indices))
         ax_y = [i_split + 0.5] * len(indices)
         ax.scatter(
-            ax_x, ax_y, c=indices, marker="_", lw=10, cmap=cmap_cv, vmin=-0.2, vmax=1.2
+            ax_x, ax_y, c=colors, marker="_", lw=10
         )
 
     # Set y-ticks and labels
@@ -46,7 +52,7 @@ def plot_kfold(cv, X, y, ax, n_splits, xlim_max=100):
     # Set plot title and create legend
     ax.set_title("KFold", fontsize=14)
     legend_patches = [
-        Patch(color=cmap_cv(0.8), label="Testing set"),
-        Patch(color=cmap_cv(0.02), label="Training set"),
+        Patch(color=validation_color, label="Validation set"),
+        Patch(color=training_color, label="Training set"),
     ]
     ax.legend(handles=legend_patches, loc=(1.03, 0.8))
